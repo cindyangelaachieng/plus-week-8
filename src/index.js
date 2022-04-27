6f6c7721765d5e516b8582c932cde9026f99d31d
cindyangelaachieng <cindyangelaachieng@gmail.com>
2022-04-27 22:12:07 +0200

    added API call function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,8 @@ function formatDate(timestamp) {
   return `${day}, ${monthDay} ${month} \n ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row third-row">`;
@@ -55,6 +56,12 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "bc8c015e37beb9dba84c5e94d482acec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function enterCity(event) {
@@ -97,6 +104,8 @@ function currentWeather(weather) {
     `http://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", weather.data.weather[0].description);
+
+  getForecast(weather.data.coord);
 }
 
 let citySearch = document.querySelector("#city-form");
@@ -145,7 +154,6 @@ function showCelTemp(event) {
 }
 
 let celTemp = null;
-displayForecast();
 
 let currentCity = document.querySelector("#current-form");
 currentCity.addEventListener("submit", getPosition);
