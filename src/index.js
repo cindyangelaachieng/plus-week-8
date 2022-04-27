f5dca7504d73b3f5d247ad789ef34e094b62484e
cindyangelaachieng <cindyangelaachieng@gmail.com>
2022-04-27 22:47:48 +0200

    forecast data has been added with API

--- src/index.js
+++ src/index.js
@@ -38,20 +38,36 @@ function formatDate(timestamp) {
   return `${day}, ${monthDay} ${month} \n ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row third-row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2 future">
-        <div class="row days">${day}</div>
-        <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" class="forecast-img" id="forecast-img" width = "42">
-        <div class="row days-temp"><span class="high">18</span><span class="low">12</span></div>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2 future">
+        <div class="row days">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" class="forecast-img" id="forecast-img" width = "42">
+        <div class="row days-temp"><span class="high">${Math.round(
+          forecastDay.temp.max
+        )}&deg</span><span class="low">${Math.round(
+          forecastDay.temp.min
+        )}&deg</span></div>
         </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
